927cc4de58f21c847cbc7772e7f3cb6d532dc8df
sergonzalezgomez <43619335+sergonzalezgomez@users.noreply.github.com>
2019-02-06 13:55:23 +0100

    01234

---
+++ EquipoBaloncesto.java
@@ -0,0 +1,10 @@
+
+public class EquipoBaloncesto
+{
+    /**
+     * Constructor for objects of class EquipoBaloncesto
+     */
+    public EquipoBaloncesto(){
+
+    }
+}
---
+++ JugadorBaloncesto.java
@@ -0,0 +1,65 @@
+import java.time.LocalDate;
+
+public class JugadorBaloncesto
+{
+    private String nombreJugador;
+    private int alturaJugador;
+    private LocalDate fechaNacimiento;
+    private boolean juegaNba;
+    private int idJugador;
+
+    public JugadorBaloncesto(String nombre, int altura, String nacimiento, boolean jugadorNba, int id){
+        nombreJugador = nombre;
+        alturaJugador = altura;
+        fechaNacimiento = LocalDate.parse(nacimiento);
+        juegaNba = jugadorNba;
+        idJugador = id;
+    }
+
+    public String getNombreJugador(){
+        return nombreJugador;
+    }
+
+    public int getAlturaJugador(){
+        return alturaJugador;
+    }
+
+    public LocalDate getFechaNacimiento(){
+        return fechaNacimiento;
+    }
+
+    public String getJuegaNba(){
+        String cadenaADevolver = "";
+        if (juegaNba){
+            cadenaADevolver = "Sí juega en la NBA";
+        }
+        else{
+            cadenaADevolver = "No juega en la NBA";
+        }
+        return cadenaADevolver;
+    }
+
+    public void setNombre(String nombre){
+        nombreJugador = nombre;
+    }
+
+    public void setAlturaJugador(int altura){
+        alturaJugador = altura;
+    }
+
+    public void setFechaNacimiento(String nacimiento){
+        fechaNacimiento = LocalDate.parse(nacimiento);
+    }
+
+    public void setJuegaNba(boolean jugadorNba){
+        juegaNba = jugadorNba;
+    }
+
+    public String caracteristicas(){
+        String caracteristicasADevolver = getNombreJugador() + " " +
+            getAlturaJugador() + " " +
+            getFechaNacimiento() + " " +
+            getJuegaNba() + " " + idJugador;
+        return caracteristicasADevolver;
+    }
+}
